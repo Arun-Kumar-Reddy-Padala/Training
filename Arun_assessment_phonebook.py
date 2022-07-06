e2843bc5dc99c21e28c75e7ba64f3cf0c49defa9
Arun-Kumar-Reddy-Padala <107461588+Arun-Kumar-Reddy-Padala@users.noreply.github.com>
2022-07-06 13:06:41 +0530

    Update Arun_assessment_phonebook.py

--- Arun_assessment_phonebook.py
+++ Arun_assessment_phonebook.py
@@ -11,31 +11,34 @@ while menu!=5:
 4. Search Contact
 5. Exit
 	""")
-	menu=int(input("Enter your option: "))
-	if (menu==1):
-		temp=[]
-		first_name = input("Enter the First Name: ")
-		last_name = input("Enter the Last Name: ")
-		mobile_number = int(input("Enter the mobile number: "))
-		temp.append(first_name)
-		temp.append(last_name)
-		temp.append(mobile_number)
-		phonebook[first_name]=temp
-	elif (menu==2):
-		delete=input("Enter the contact's first name you want to delete:" )
-		if delete in phonebook:
-			phonebook.pop(first_name)
-			print(f"Contact {delete} is deleted")
-		else:
-			print("Selected Contact is not in the phonebook.")
-	elif (menu==3):
-		print(phonebook)
+	try:
+		menu=int(input("Enter your option: "))
+		if (menu==1):
+			temp=[]
+			first_name = input("Enter the First Name: ")
+			last_name = input("Enter the Last Name: ")
+			mobile_number = int(input("Enter the mobile number: "))
+			temp.append(first_name)
+			temp.append(last_name)
+			temp.append(mobile_number)
+			phonebook[first_name]=temp
+		elif (menu==2):
+			delete=input("Enter the contact's first name you want to delete:" )
+			if delete in phonebook:
+				phonebook.pop(first_name)
+				print(f"Contact {delete} is deleted")
+			else:
+				print("Selected Contact is not in the phonebook.")
+		elif (menu==3):
+			print(phonebook)
 
-	elif (menu==4):
-		search=input("Enter the contact's first name you want to search: ")
-		if search in phonebook:
-			print(phonebook[search])
-		else:
-			print("There is no contact named "+search)
-	elif (menu==5):
-		sys.exit()
+		elif (menu==4):
+			search=input("Enter the contact's first name you want to search: ")
+			if search in phonebook:
+				print(phonebook[search])
+			else:
+				print("There is no contact named "+search)
+		elif (menu==5):
+			sys.exit()
+	except ValueError:
+		print("Please enter a valid number.")
